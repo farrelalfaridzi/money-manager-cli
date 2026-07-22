58fb9cd0b200a1f12eea0838bd0e01d8c9a5b24f
Farrel <farrel.alfaridziii@gmail.com>
2026-07-22 14:14:48 +0700

    feat: integrate transaction form with SQLite

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from core.transaction import Transaction
+from core.manager import TransactionManager
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    manager = TransactionManager()
+    if request.method == "POST":
+        jumlah = int(request.form["amount"])
+        kategori = request.form["category"]
+        catatan = request.form["description"]
+        jenis = request.form["jenis"]
+        transaction = Transaction(jumlah, kategori, catatan, jenis)
+        manager.add_transaction(transaction)
+    return render_template("add.html")
 
 if __name__=="__main__":
     app.run(debug=True)
